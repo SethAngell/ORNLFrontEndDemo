d26142eee7977751851f98d3d7326b5b05b3a0e8
SethAngell <Seth@probit.live>
2019-04-30 16:26:39 -0400

    Now with 100% less git lfs

---
+++ Assets/VPS.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VPS : MonoBehaviour
+{
+/*
+    public GameObject Globe;
+    public GameObject Pointer;
+    public Canvas testyBoi;
+    public Text Coordinates;
+
+    double Latitude = (24.555059 * Mathf.PI) / 180;
+    double Longitude = (-81.779984 * Mathf.PI) / 180;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void VirtualPositioningUnit()
+    {
+        double RelationalValue = 6371 / Globe.GetComponent<SphereCollider>().radius;
+        float RelationalX = (6371 * Mathf.Cos((float)Latitude) * Mathf.Cos((float)Longitude)) / RelationalValue;
+        double RelationalY = (6371 * Mathf.Cos((float)Latitude) * Mathf.Sin((float)Longitude)) / RelationalValue;
+        double RelationalZ = (6371 * Mathf.Sin((float)Latitude)) / RelationalValue;
+
+        Vector3 GlobePosition = Globe.GetComponent<GameObject>().transform.position;
+        Vector3 CubePosition = new Vector3( ((float) GlobePosition[0] + RelationalX) , (GlobePosition)
+    }
+
+    */
+}
